eea10e9d20a98302201643698bf089abe4c7876e
Grape_glb <grapefruit_freedom_glb@163.com>
2021-08-08 11:34:03 +0800

    这个c文件是判断输入的数是否为素数

--- Exercise-8/erexcise-8/erexcise-8/exercise.c
+++ Exercise-8/erexcise-8/erexcise-8/exercise.c
@@ -1,6 +1,25 @@
-//
+//判断是否是素数
 #include <stdio.h>
+
 int main() {
+
+	int x,i;
+	int judge = 1;
+	printf("判断输入的数字是是否是素数,请输入：");
+	scanf_s("%d", &x);
+
+	for (i = 2; i < x; i++) {
+		if (x % i == 0) {
+			judge = 0;
+			break;
+		}
+	}
+	if (judge == 1) {
+		printf("%d是素数!",x);
+	}
+	else {
+		printf("%d不是素数!",x);
+	}
 
 	return 0;
 }
